cd55831913165391ba1d1b6ec62e010541f23095
matheus <matheusjcunha@gmail.com>
2021-07-24 17:14:05 -0300

    Implementado page1 com ListView

--- lib/pages/page1.dart
+++ lib/pages/page1.dart
@@ -4,11 +4,21 @@ class Page1 extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: _appBar()
+        appBar: _appBar(),
+        body: _body()
     );
   }
 
   //Toolbar
   _appBar() => AppBar(title: Text("Page 1"));
+
+  _body() =>
+      ListView(
+        children: [
+          Image.asset("assets/images/dog1.png"),
+          Image.asset("assets/images/dog2.png"),
+          Image.asset("assets/images/dog3.png")
+        ],
+      );
 
 }
