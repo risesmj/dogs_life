1076b9708fb894c7ba85091b9c8f8bebcb6918ac
matheus <matheusjcunha@gmail.com>
2021-07-24 18:01:14 -0300

    Implementado lista dinâmica na page 1

--- lib/pages/page1.dart
+++ lib/pages/page1.dart
@@ -3,22 +3,32 @@ import 'package:flutter/material.dart';
 class Page1 extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-        appBar: _appBar(),
-        body: _body()
-    );
+    return Scaffold(appBar: _appBar(), body: _body());
   }
 
   //Toolbar
   _appBar() => AppBar(title: Text("Page 1"));
 
-  _body() =>
-      ListView(
-        children: [
-          Image.asset("assets/images/dog1.png"),
-          Image.asset("assets/images/dog2.png"),
-          Image.asset("assets/images/dog3.png")
-        ],
-      );
+  _body() {
+
+    final List<Image> imgs = [
+      _img("assets/images/dog1.png"),
+      _img("assets/images/dog2.png"),
+      _img("assets/images/dog3.png")
+    ];
+    
+    return ListView.builder(
+        itemCount: imgs.length,
+        itemExtent: 300,
+        itemBuilder: (BuildContext context, int index) {
+      return imgs[index];
+    });
+  }
+
+  _img(path) =>
+    Image.asset(
+      path,
+      fit: BoxFit.cover,
+    );
 
 }
